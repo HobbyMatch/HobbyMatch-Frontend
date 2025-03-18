5419efd1810f9c93075564414b2028a21d36feee
molczane <ernest.molczan@gmail.com>
2025-03-19 00:10:35 +0100

    Created edge to edge look of application on Android.

--- composeApp/src/androidMain/kotlin/io2/hobbymatch/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/io2/hobbymatch/MainActivity.kt
@@ -4,11 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Make content fill the screen
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        // Set system UI colors and appearance to have edge to edge apperance
+        val window = window
+        window.statusBarColor = Color.Transparent.toArgb()
+        window.navigationBarColor = Color.Transparent.toArgb()
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = true
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightNavigationBars = true
+
 
         setContent {
             App()
