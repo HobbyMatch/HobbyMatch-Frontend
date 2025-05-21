bdfdc95ac8e6ff44710743fe8e1c457f281359c1
molczane <ernest.molczan@gmail.com>
2025-05-21 18:14:46 +0200

    Integrate MaplibreMap and remove Cocoapods plugin
    Replaced the welcome text in `HomeScreen` with `MaplibreMap` for map display. Removed the `kotlinCocoapods` plugin from `build.gradle.kts` and `gradle/libs.versions.toml`, and commented out its configuration in `composeApp/build.gradle.kts`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,5 +9,4 @@ plugins {
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
-    alias(libs.plugins.kotlinCocoapods) apply false
 }
