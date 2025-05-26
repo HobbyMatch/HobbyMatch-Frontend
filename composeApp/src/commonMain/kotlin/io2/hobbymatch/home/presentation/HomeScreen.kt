3a88788fbac0f00634737f92d4b2a04f252e34c3
molczane <ernest.molczan@gmail.com>
2025-05-26 09:30:04 +0200

    Temporarily remove MaplibreMap from HomeScreen
    The MaplibreMap component has been commented out in the HomeScreen.
    Additionally, a trailing newline was removed from `getAuthRoomDatabase.kt`.

--- composeApp/src/commonMain/kotlin/io2/hobbymatch/home/presentation/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/io2/hobbymatch/home/presentation/HomeScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
-import dev.sargunv.maplibrecompose.compose.MaplibreMap
 
 class HomeScreen : Screen, Tab {
     override val options: TabOptions
@@ -52,7 +51,7 @@ class HomeScreen : Screen, Tab {
                     .padding(horizontal = 16.dp), // Add horizontal padding
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                MaplibreMap()
+                // MaplibreMap()
             }
         }
     }
